258633fdad1b8bb37e0b2ee0937a78d5cb33c61d
changuikim <cu.kim.dev@gmail.com>
2024-06-18 02:01:10 +0900

    JWT

--- src/app/apis/fetchMainPage/getRoomsList.ts
+++ src/app/apis/fetchMainPage/getRoomsList.ts
@@ -14,6 +14,7 @@ async function getRoomsList(id, fields) {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.JWT_TOKEN}`,
         },
 
         // cache: 'no-store',
